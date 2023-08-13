4401fe5f319a55f53563901c03953a52ff2b3cc7
Collins Daberechukwu <107154225+Collinsdaberechukwu@users.noreply.github.com>
2023-08-13 20:34:26 +0100

    Add files via upload
    Twitter_Clone_Project

---
+++ data.js
@@ -0,0 +1,82 @@
+export const tweetsData = [   
+    {
+        handle: `@Daberechukwu 💎`,
+        profilePic: `pictures/decagon.jpeg`,
+        likes: 27000,
+        retweets: 10,
+        tweetText: `"🚀 Exciting times in the tech world! Just launched our startup aiming to revolutionize [industry/sector]. Innovation is
+         our middle name. 👩‍💻👨‍💻 Let's shape the future together! #TechStartup #Innovation"`,
+        replies: [
+            {
+                handle: `@TechAfrica ✅`,
+                profilePic: ``,
+                tweetText: `🙌 Congrats on the launch! 🎉 Can't wait to see how you'll change the game. #FutureTech" 😎🛩`,
+            },
+                  {
+                handle: `@Software_Zeb ✅`,
+                profilePic: ``,
+                tweetText: `🌐 The startup scene is buzzing! Wishing you success and groundbreaking 
+                achievements. 🌟 #StartupAdventures"`,
+            },
+
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Scrimba ✅`,
+        profilePic: `pictures/scrimbalogo.png`,
+        likes: 23600,
+        retweets: 5234,
+        tweetText: `"📚🌟 Level up your coding skills with @scrimba! 🖥️💡 Interactive coding tutorials
+         that let you learn by doing. Say goodbye to passive learning and hello to hands-on experience. 💪🚀 Start your 
+         coding journey today and unlock a world of endless possibilities. Share your favorite Scrimba course below! 👇 #Scrimba
+          #InteractiveLearning #CodeWithConfidence"`,
+        replies: [
+                  {
+                handle: `@Tech ✅`,
+                profilePic: ``,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@Tolu ✅`,
+                profilePic: `pictures/girls.jpeg`,
+                tweetText: `"Couldn't agree more! @scrimba's approach to learning is spot on. Finally mastering those 
+                coding challenges I used to struggle with. 🚀👩‍💻 #CodeSuccess #Scrimba"😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+    
+        {
+        handle: `@Decagon`,
+        profilePic: `pictures/deca.png`,
+        likes: 289450,
+        retweets: 1719,
+        tweetText: `🚀🇳🇬 Leading the tech education revolution in Nigeria, @DecagonHQ stands tall as the epitome of excellence! 🎓💡 With industry-aligned curriculum, 
+        top-notch mentors, and real-world projects, they're shaping the best tech talents. Who else is proud to be a part of the 
+        #DecagonJourney? 🙌🌟 #TechEducation ?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `"Couldn't have asked for a better tech institute than @DecagonHQ. They're not just teaching skills,
+                 but also fostering a community of innovation and growth.
+                 💪🌐 #DecagonFamily #TechInnovation"`,
+            },
+            {
+                handle: `@Grace_29`,
+                profilePic: `images/love.png`,
+                tweetText: `"Absolutely loving my journey at @DecagonHQ! The hands-on experience and exposure to industry demands 
+                are unmatched. Proud to be learning from the best! 
+                💡🚀 #DecagonMagic #TechFuture"`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
